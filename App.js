e69ebf8826b6fb0ca10e27a61c78958dcd2e642d
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-02 13:53:26 -0500

    Created menu

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-//With defer ti is better to wait for the event of DOMContentLoaded to occur before manipulation
+import Store from './services/Store.js';
+import API from './services/API.js';
+import { loadData } from './services/Menu.js';
+
+// //With defer ti is better to wait for the event of DOMContentLoaded to occur before manipulation
+// window.addEventListener('DOMContentLoaded', () => {
+//   let nav = document.querySelector('nav');
+//   console.log(nav.innerText);
+//   nav.innerHTML = `<h2>Hello DOM!</h2>`;
+// });
+
+// //When does this run?
+
+// //Alias or wrapper for querySelector can be $ or _, like in jQuery
+
+// const $ = () => document.querySelector.call(this, arguments);
+// const _ = {
+//   domready: (e) => {
+//     console.log(e);
+//   },
+// };
+// HTMLElement.prototype.$ = (s) => this.querySelector(s);
+// _.domready('test');
+
+//Adding services to make gloabl store
+
 window.addEventListener('DOMContentLoaded', () => {
-  let nav = document.querySelector('nav');
-  console.log(nav.innerText);
-  nav.innerHTML = `<h2>Hello DOM!</h2>`;
+  console.log(App);
+  loadData();
 });
 
-//When does this run?
-
-//Alias or wrapper for querySelector can be $ or _, like in jQuery
-
-const $ = () => document.querySelector.call(this, arguments);
-const _ = {
-  domready: (e) => {
-    console.log(e);
-  },
-};
-HTMLElement.prototype.$ = (s) => this.querySelector(s);
-_.domready('test');
+window.App = {};
+App.store = Store;
---
+++ services /Menu.js
@@ -0,0 +1,5 @@
+import API from '.API.js';
+
+export async function loadData() {
+  App.store.menu = await API.fetchMenu();
+}
